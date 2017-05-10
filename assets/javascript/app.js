681670b6281ed40a4442b616094470e4b9274a2a
Margaret Choi <margaret.j.li@gmail.com>
2017-05-10 01:05:57 -0700

    Change question set to an array, fix double timer bug

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -23,56 +23,60 @@ var correct = 0;
 var incorrect = 0;
 
 // Question objects
-var questionSet = {
-	1: {
+var q1 = {
 		text: "The giant panda's diet is 99% this.",
 		option1: "Bamboo",
 		option2: "Bananas",
 		option3: "Honey",
 		option4: "Fish",
 		answer: "Bamboo"
-	},  
+	} 
 
-	2: {
+var q2 = {
 		text: "Pandas live in the mountains in the Sichuan, Shaanxi, and Gansu provinces in this country.",
 		option1: "China",
 		option2: "Thailand",
 		option3: "Mongolia",
 		option4: "Korea",
 		answer: "China"
-	},
+	}
 
-	3: {
+var q3 = {
 		text: "At birth, pandas weigh about 4 ounces and are the size of a stick of butter. Which of these is NOT true about a newborn panda?",
 		option1: "They are grey in color.",
 		option2: "They are hairless.",
 		option3: "They nurse up to 14 times a day.",
 		option4: "They are blind.",
 		answer: "They are grey in color."
-	},
+	}
 
-	4: {
-		text: "Pandas do not hibernate.",
-		option1: "True",
-		option2: "False",
-		answer: "True"
-	},
+var q4 = {
+		text: "A group of pandas is called",
+		option1: "A posse",
+		option2: "A pack",
+		option3: "An embarassment",
+		option4: "A herd",
+		answer: "An embarassment"
+	}
 
-	5: {
-		text: "Pandas live and travel together.",
-		option1: "True",
-		option2: "False",
-		answer: "False"
-	},
-	6: {
+var q5 = {
+		text: "How many fingers does a Giant Panda have on a single paw?",
+		option1: "4",
+		option2: "5",
+		option3: "6",
+		option4: "7",
+		answer: "6"
+	}
+
+var q6 = {
 		text: "Pandas spend 10-16 hours a day eating, the rest of the time they are usually resting. When are pandas most active?",
 		option1: "Between 2p and 10pm",
 		option2: "Only at night",
 		option3: "Between 10am and 2pm",
 		option4: "At dusk and at dawn",
 		answer: "At dusk and at dawn"
-	},
-	7: {
+	}
+var q7 = {
 		text: "When a panda is born, it is impossible to tell if it is male or female. How old is the panda before its gender is known?",
 		option1: "3 months",
 		option2: "4 years",
@@ -80,31 +84,31 @@ var questionSet = {
 		option4: "1 year",
 		answer: "4 years"
 
-	},
-	8: {
+	}
+var q8 = {
 		text: "Which of these is TRUE about pandas?",
 		option1: "They belong to the raccoon family.",
 		option2: "They are carnivores.",
 		option3: "They were unknown to the Western world until the 1600s. ",
 		option4: "They are poor climbers.",
 		answer: "They are carnivores."
-	},
-	9: {
+	}
+var q9 = {
 		text: "How long does a panda usually live in captivity?",
 		option1: "80 - 90 years",
 		option2: "55 - 65 years",
 		option3: "10 - 15 years",
 		option4: "20 - 30 years",
 		answer: "20 - 30 years"
-	},
-	10: {
+	}
+var q10 = {
 		text: "The panda has an enlarged wristbone that works as an opposable thumb.",
 		option1: "True",
 		option2: "False",
 		answer: "True"
-	},
-}
+	}
 
+var questionSet = [q1, q2, q3, q4, q5, q6, q7, q8, q9, q10];
 
 // New trivia
 $(document).ready(function() {
@@ -114,10 +118,11 @@ $(document).ready(function() {
 	var x = "<button id='start-btn'>Start Game</button>";
 	$('#trivia').append(x);
 	$('#start-btn').click(startGame);
-	// var startButton = $('#start-btn');
-	// $('input:radio').val('question');
+
 });
 
+
+// Starts game on click of start button
 function startGame() {
 	console.log('start');
 
@@ -129,6 +134,10 @@ function startGame() {
 
 	// Hide the start button after it's clicked
 	$('#start-btn').hide();
+
+	var x = "<button id='restart-btn'>Restart</button>";
+	$('#restart-box').append(x);
+	$('#restart-btn').click(function(){location.reload();});
 }
 
 function firstQuestion() {
@@ -158,38 +167,78 @@ function firstQuestion() {
 }
 
 var x;
-x = 1;
+x = 0;
 
+var countDown;
 
 function loadQuestion() {
+
+	t = 20;
+	$('#timer-box').html(t + ' seconds');
+	var countDown = setInterval(timerCount, 1000);
+
+	function timerCount() {
+		t--; 
+		$('#timer-box').html(t + ' seconds');
+
+		// Stop timer at 0
+		if (t <= 0) {
+			// var x;
+			$('#reveal-box').html("You're out of time! The correct answer is ");
+			$('#timer-box').html("0 seconds");
+			incorrect++;
+			$('#scores-box').html('Correct: ' + correct + '<br> Incorrect: ' + incorrect);
+					
+			setInterval(nextQuestion(), 3000);
+
+			clearInterval(countDown);
+		}
+
+	}
+
 	$('#question-box').html(questionSet[x].text);
 	// Create radio button for each question
-	$('#option-box').html('<input type="radio" name="action" id="option1">' + questionSet[x].option1 
-		+ '<br>');
-	$('#option1').val(questionSet[x].option1);
-	$('#option-box').append('<input type="radio" name="action"  id="option2">' + questionSet[x].option2 
-		+ '<br>');
-	$('#option2').val(questionSet[x].option2);
-	$('#option-box').append('<input type="radio" name="action" id="option3">' + questionSet[x].option3 + '<br>');
-	$('#option3').val(questionSet[x].option3);
-	$('#option-box').append('<input type="radio" name="action"  id="option4">' + questionSet[x].option4 + '<br>');
-	$('#option4').val(questionSet[x].option4);
+	$('#option-box').html('<li id="option1" class="answer">' + questionSet[x].option1
+		+ '</li><br>');
+	// $('#option1').val(questionSet[x].option1);
+	$('#option-box').append('<li id="option2" class="answer">' + questionSet[x].option2 
+		+ '</li><br>');
+	// $('#option2').val(questionSet[x].option2);
+	$('#option-box').append('<li id="option3" class="answer">' + questionSet[x].option3 
+		+ '</li><br>');
+	// $('#option3').val(questionSet[x].option3);
+	$('#option-box').append('<li id="option4" class="answer">' + questionSet[x].option4 
+		+ '</li><br>');
+	// $('#option4').val(questionSet[x].option4);
 
-			// Submits the form when a radio button is clicked
-	$('input[type=radio]').on('change', function() {
+	$('#reveal-box').html('');
+
+	// Changes when the link is clicked
+	$('.answer').on('click', function() {
 
 		// If the clicked button matches the answer, show correct
-		if (this.value === questionSet[x].answer) {
+		if (this.innerHTML === questionSet[x].answer) {
 			$('#reveal-box').html("Correct!");
 			correct++;
 			$('#scores-box').html('Correct: ' + correct + '<br> Incorrect: ' + incorrect);
+			
+			
+			$(this).attr('background', '#000');
+			$('.answer').off('click');
+			clearInterval(countDown);
 			setTimeout(nextQuestion, 3000);
 		}
-		// Adds the correct answer to the reveal box
+
+		// If the clicked button  doesn't match, adds the correct answer to the reveal box
 		else {
 			$('#reveal-box').html("Wrong, the correct answer is " + questionSet[x].answer);
 			incorrect++;
 			$('#scores-box').html('Correct: ' + correct + '<br> Incorrect: ' + incorrect);
+			
+			
+			$(this).attr('background', '#000');
+			$('.answer').off('click');
+			clearInterval(countDown);
 			setTimeout(nextQuestion, 3000);
 		}
 	
@@ -200,35 +249,60 @@ function loadQuestion() {
 
 	});
 
-	timer();
+	
 
 }
 
 function nextQuestion() {
-	console.log('nextup');
 	x = x + 1;
-	loadQuestion();
-}
 
-function timer() {
-	t = 30;
-	$('#timer-box').html('00:30');
-	var countDown = setInterval(timerCount, 1000);
-}
+	if (x === 10) {
+		console.log(10);
+		endGame(); 
+	}
 
-function timerCount() {
-	t--; 
-	$('#timer-box').html(t);
-
-	// Stop timer at 0
-	if (t <= 0) {
-		clearInterval(countDown);
-		var x;
-		$('#reveal-box').html("You're out of time! The correct answer is " + questionSet[x].answer);
-		nextQuestion();
-		console.log('done');
+	else {
+		loadQuestion();
 	}
 }
+
+
+function endGame() {
+	console.log('end');
+	$('#trivia').html('<h2>Good game!</h2> Correct: ' + correct + '<br> Incorrect: ' 
+		+ incorrect);
+	var x = "<button id='restart-btn'>Restart</button>";
+	$('#trivia').append(x);
+	$('#restart-btn').click(function(){location.reload();});
+}
+
+
+// function timer() {
+// 	t = 20;
+// 	$('#timer-box').html(t + ' seconds');
+// 	var countDown = setInterval(timerCount, 1000);
+
+// 	function timerCount() {
+// 		t--; 
+// 		$('#timer-box').html(t + ' seconds');
+
+// 		// Stop timer at 0
+// 		if (t <= 0) {
+// 			// var x;
+// 			$('#reveal-box').html("You're out of time! The correct answer is ");
+// 			$('#timer-box').html("0 seconds");
+// 			incorrect++;
+// 			$('#scores-box').html('Correct: ' + correct + '<br> Incorrect: ' + incorrect);
+					
+// 			setInterval(nextQuestion(), 3000);
+
+// 			clearInterval(countDown);
+// 		}
+
+// 	}
+
+// }
+
 
 
 
